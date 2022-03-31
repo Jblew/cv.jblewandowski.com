2ece8569ff557cec8f8a12a29b7781091797f4f9
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-04-01 00:40:57 +0200

    feat: build monified and expanded css

--- build-sass.js
+++ build-sass.js
@@ -1,5 +1,6 @@
 const fs = require("fs")
 const sass = require("sass")
-const result = sass.compile("scss/index.scss", { style: "compressed" });
-const outCss = result.css
-fs.writeFileSync("style.css", outCss)
+const result = sass.compile("scss/index.scss", { style: "expanded" });
+fs.writeFileSync("style.css", result.css)
+const resultCompressed = sass.compile("scss/index.scss", { style: "compressed" });
+fs.writeFileSync("style.min.css", resultCompressed.css)
